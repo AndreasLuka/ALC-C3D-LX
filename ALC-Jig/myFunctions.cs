e89b2be7a176ebeafc5f553f9e902de5599fc58a
AndreasLuka <andreas@luka-online.de>
2018-08-03 12:12:40 -0700

    Test files removed

--- ALC-Jig/myFunctions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Geometry;
-using Autodesk.AutoCAD.ApplicationServices;
-
-namespace ALC_Jig
-{
-
-}
